eaa394d9e11e0711934b01ad9a10f6494101e471
Kaizu-r <jljoloc@gmail.com>
2025-03-24 01:02:52 +0800

    UPD: Added tokenizer

--- parse.h
+++ parse.h
@@ -1,12 +1,5 @@
 #include<stdio.h>
 
-char* readString(){
-    char* str;
-    scanf("%c[^\n]%*c", str);
-
-    return str;
-}
-
 void removeSpace(char* str){
     char* str2 = str;
     do{
@@ -14,4 +7,40 @@ void removeSpace(char* str){
             str2++;
     }while(*str++ = *str2++);
     str = '\0';
+}
+
+void tokenize(int* arr, char* str){ //push an int to arr depending on the symbol
+    int i = 0;
+    while(str[i] != '\0'){
+        switch(str[i]){
+            case '+':
+            case '-':
+            case '/':
+            case '*':
+                arr[i] = 1;
+                break;
+            case '(':
+                arr[i] = 2;
+                break;
+            case ')':
+                arr[i] = 3;
+                break;
+            case'1':
+            case'2':
+            case'3':
+            case'4':
+            case'5':
+            case'6':
+            case'7':
+            case'8':
+            case'9':
+            case'0':
+                arr[i] = 4;
+                break;
+            default:
+                arr[i] = 0; //for error
+            
+        }
+        i++;
+    }
 }
