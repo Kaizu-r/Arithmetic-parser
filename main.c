9400fd26ced90b514cc721dd86b8a06dd46af496
Kaizu-r <jljoloc@gmail.com>
2025-03-25 21:05:24 +0800

    TEST: Tokenizer function

--- main.c
+++ main.c
@@ -2,16 +2,27 @@
 #include <string.h>
 #include "parse.h"
 
-int main(){
+int main()
+{
     printf("Enter: ");
     char str[100];
+    char terminals[100];
     int arr[100] = {0};
     fgets(str, 100, stdin);
     printf(str);
     removeSpace(str);
     printf(str);
-    tokenize(arr, str);
-    for(int i = 0; i < strlen(str) -1; i++)
-        printf("%d", arr[i]);
+    
+    Token t[100];
+    if(tokenize(str, t))
+        printf("FAILED\n");
+
+    int j = 0;
+    while(t[j].token != END){
+        printf("%d ", t[j].token);
+        j++;
+    }
+    printf("%d ", t[j].token);
+
     return 0;
 }
