0404870f79378da7c5834d885e12751ab7805544
Kaizu-r <jljoloc@gmail.com>
2025-03-26 17:53:45 +0800

    TEST: all functions working as intended

--- main.c
+++ main.c
@@ -4,21 +4,41 @@
 
 int main()
 {
-    printf("Enter: ");
     char str[100];
-    char terminals[100];
-    int arr[100] = {0};
-    fgets(str, 100, stdin);
-    printf(str);
-    removeSpace(str);
-    printf(str);
-    
     Token t[100];
+    float numbers[100];
+    Token_t queue[100];
+    int rear = -1;
+
+    printf("Enter: ");
+    fgets(str, 100, stdin);
+    removeSpace(str);
+    
     if(tokenize(str, t))
         printf("FAILED\n");
-    printf("x\n");
     if(isBalanced(t))
         printf("BALANCED\n");
+   
+    toRPN(str, numbers, queue, t, &rear);
+
+    for(int i = 0, j = 0; i <= rear; i++)
+    {
+        if(queue[i] == DIGIT)
+            printf("%f ", numbers[j++]);
+        else if(queue[i] == ADD)
+            printf("+ ");
+        else if(queue[i] == SUBTRACT)
+            printf("- ");
+        else if(queue[i] == MULTIPLY)
+            printf("* ");
+        else if(queue[i] == DIVIDE)
+            printf("/ ");
+        else
+            printf("e ");
+    }
+
+    float answer = solve(numbers, queue, rear);
+    printf("\nResult: %f\n", answer);
 
     return 0;
 }
