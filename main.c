8f16dba2cae8e16a7bdabe8098e5a6d3bf84ba3e
Kaizu-r <jljoloc@gmail.com>
2025-04-15 15:38:13 +0800

    UPD: Improved visuals

--- main.c
+++ main.c
@@ -1,68 +1,83 @@
 #include<stdio.h>
 #include <string.h>
-#include "parse.h"
+#include "parser.h"
+#include "solver.h"
+#include "error.h"
 
 int main()
 {
     char str[100];
-    Token t[100];
+    Token t[100];   //token array
     float numbers[100];
     Token_t queue[100];
+    error e;
     int rear = -1;
+    Token_t arr[100];
+    int l = 0;  //counter for token_t array
+    Token *k = t;   //pointer for parsing
+    float answer;
 
-    printf("Enter: ");
+    printf("====ARITHMETIC PARSER====\n\n");
+    printf("Enter string: ");
     fgets(str, 100, stdin);
+    printf("\nRemoving spaces...\n");
     removeSpace(str);
     
-    if(tokenize(str, t))
-        printf("FAILED\n");
-
-    if(isBalanced(t))
-        printf("BALANCED\n");
-   
-    Token_t arr[100];
-    int l = 0;
-    Token *k = t;
+    printf("\nTokenizing...\n");
+    if(tokenize(str, t)){
+        printf("FAILED!!! Exiting progran...\n");
+        return 1;
+    }
+    printf("Tokenizing Succeful!!\n");
+    printf("Checking for balanced parenthesis....\n");
+    if(!isBalanced(t, &e)){
+        printError(e);
+        return 1;
+    }
+   printf("Balanced parenthesis\n\n");
+    
     while(k->token != END){
         arr[l++] = k++->token;
     }
     arr[l] = END;
-    
+    printf("Token String: ");
     for(l = 0; arr[l] != END; l++){
         printToken(arr[l]);
     }
 
 
 
+    printf("\nParsing...\n");
 
-    if(!validate(arr)){
-        printf("\n String not Valid\n");
-        return 0;
+    if(!validate(arr, &e)){
+        printError(e);
+        return 1;
     }
+    printf("Valid string!!\n");
 
-
-
+    printf("Converting to Reverse Polish Notation...\n");
     toRPN(str, numbers, queue, t, &rear);
 
+
+    printf("Reverse Polish: ");
     for(int i = 0, j = 0; i <= rear; i++)
     {
         if(queue[i] == DIGIT)
             printf("%f ", numbers[j++]);
-        else if(queue[i] == ADD)
-            printf("+ ");
-        else if(queue[i] == SUBTRACT)
-            printf("- ");
-        else if(queue[i] == MULTIPLY)
-            printf("* ");
-        else if(queue[i] == DIVIDE)
-            printf("/ ");
-        else if (queue[i] == POW)
-            printf("^ ");
-        else if (queue[i] == MOD)
-            printf("%% ");
+        else
+            printNonDigitToken(queue[i]);
+
     }
 
-    float answer = solve(numbers, queue, rear);
+    printf("\n");
+    
+    printf("\nSOLVING EXPRESSION...\n");
+    if(solve(&answer, numbers, queue, rear, &e)){
+        printf("\n");
+        printError(e);
+        return 1;
+    }
+
     printf("\nResult: %f\n", answer);
 
     return 0;
